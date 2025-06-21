a96b9be4b5074e6dc792b670a7e6b692dcc14b05
nuzrulislamff <nuzrulislamff@gmail.com>
2025-06-21 13:12:21 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import HomeScreen from "./components/HomeScreen";
-import WalletConnect from "./components/WalletConnect";
 
 export default function App() {
-  return (
-    <div>
-      <WalletConnect />
-      <HomeScreen />
-    </div>
-  );
+  return <HomeScreen />;
 }

a2b76b842dc9a8204f7635442fdf9673eb7066b9
nuzrulislamff <nuzrulislamff@gmail.com>
2025-06-21 12:44:21 +0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HomeScreen from "./components/HomeScreen";
+
+export default function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <HomeScreen />
+    </div>
+  );
+}
